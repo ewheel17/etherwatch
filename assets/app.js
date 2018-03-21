78151b4838718edd4b1b79d95591ee368348e3cc
Max <u1110071@utah.edu>
2018-03-21 13:57:37 -0600

    first

---
+++ assets/app.js
@@ -0,0 +1,14 @@
+
+
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDmFYLuc16iNNQTPwvVBu9qkap2o4qlcQA",
+    authDomain: "etherwatch-1659d.firebaseapp.com",
+    databaseURL: "https://etherwatch-1659d.firebaseio.com",
+    projectId: "etherwatch-1659d",
+    storageBucket: "",
+    messagingSenderId: "595784562003"
+};
+
+firebase.initializeApp(config);
