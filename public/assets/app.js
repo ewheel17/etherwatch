69e4fa4d43fe65eced9c456bbb371c5d32f5f81d
Max <ewheel17@gmail.com>
2018-04-04 14:26:03 -0600

    login page setup beta

--- public/assets/app.js
+++ public/assets/app.js
@@ -4,7 +4,7 @@ var config = {
     authDomain: "etherwatch-1659d.firebaseapp.com",
     databaseURL: "https://etherwatch-1659d.firebaseio.com",
     projectId: "etherwatch-1659d",
-    storageBucket: "",
+    storageBucket: "etherwatch-1659d.appspot.com",
     messagingSenderId: "595784562003"
 };
 
---
+++ public/assets/login.js
@@ -0,0 +1,12 @@
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDmFYLuc16iNNQTPwvVBu9qkap2o4qlcQA",
+    authDomain: "etherwatch-1659d.firebaseapp.com",
+    databaseURL: "https://etherwatch-1659d.firebaseio.com",
+    projectId: "etherwatch-1659d",
+    storageBucket: "etherwatch-1659d.appspot.com",
+    messagingSenderId: "595784562003"
+};
+  
+firebase.initializeApp(config);
