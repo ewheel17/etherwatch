d0d672aee4faa9f8b1f4c8c5102d946c534e77ba
Max <ewheel17@gmail.com>
2018-04-10 15:12:10 -0600

    more login adjustments

--- public/assets/login.js
+++ public/assets/login.js
@@ -10,3 +10,25 @@ var config = {
 };
   
 firebase.initializeApp(config);
+
+//Login Auth
+$("#submit-auth").on("click", function(event){
+    event.preventDefault();
+  
+      var email = $("#email-input").val().trim();
+      var password = $("#password-input").val().trim();
+  
+      firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+  
+      });
+  
+      //Auth Check
+      firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          window.location.replace("../dashboard.html");
+        }
+      });
+  });
