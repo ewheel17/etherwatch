21eff50294d70711c85bb86d557f9d9bd5a9550a
Max <ewheel17@gmail.com>
2018-04-10 16:09:36 -0600

    Getting firebase login setup (incomplete)

--- public/assets/login.js
+++ public/assets/login.js
@@ -11,24 +11,52 @@ var config = {
   
 firebase.initializeApp(config);
 
+var database = firebase.database();
+
+$("#initialSignUp").on("click", () => {
+  var firstName = ;
+  var lastName = ;
+  var email = ;
+  var password = ;
+  
+  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+      console.log("authworked");
+    });
+});
+
 //Login Auth
 $("#submit-auth").on("click", function(event){
     event.preventDefault();
   
-      var email = $("#email-input").val().trim();
-      var password = $("#password-input").val().trim();
+    var email = $("#email-input").val().trim();
+    var password = $("#password-input").val().trim();
   
-      firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
   
-      });
+    });
   
-      //Auth Check
-      firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-          window.location.replace("../dashboard.html");
-        }
-      });
-  });
+    //Auth Check
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        window.location.replace("../dashboard.html");
+      }
+    });
+});
+
+// //Auth Check & User Log
+// var pathname = window.location.pathname;
+
+// if (pathname != "/index.html") {
+//   firebase.auth().onAuthStateChanged(function(user) {
+//     if (user) {} else {
+//       window.location.replace("../index.html");
+//     }
+//   });
+// }
